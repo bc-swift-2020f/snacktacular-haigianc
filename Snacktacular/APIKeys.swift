8a709868f1125fb8f70039f9d97542ef209c9d11
Claudine Haigian <haigianc@bc.edu>
2020-11-02 15:02:38 -0500

    Week 10 Work: Through 8.8

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Claudine Haigian on 11/2/20.
+//  Copyright © 2020 Claudine Haigian. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys{
+    static let googlePlacesKey = "AIzaSyAarAN63kLfFzJqUTOtulyP0nxbn0G_Eqk"
+}
